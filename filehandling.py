4d7e5b3632c5afef0109a95b424633ed68c4b4d1
Umesh Avaiya <uhavaiya@gmail.com>
2020-09-20 20:26:41 +0530

    functioncreate

---
+++ filehandling.py
@@ -0,0 +1,4 @@
+past =""
+for i in range(5):
+    past = past + "* "
+    print(past)
---
+++ functioncalling.py
@@ -0,0 +1,9 @@
+def num(x,y):
+  a = x+y
+  b = x-y
+  c = x*y
+  d = x/y
+  return a,b,c,d
+
+result = num(10,5)
+print(result)
